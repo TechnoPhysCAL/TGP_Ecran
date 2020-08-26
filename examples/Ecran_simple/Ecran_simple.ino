a65ae3ae27fd9a5a0c24e4aa1b96926fbabfb948
TechnoPhysCAL <claude.bouchard@claurendeau.qc.ca>
2020-08-26 09:40:03 -0400

    Ajustement/correction des exemples.

--- examples/Ecran_simple/Ecran_simple.ino
+++ examples/Ecran_simple/Ecran_simple.ino
@@ -1,18 +1,44 @@
+/*
+  Librairie TPG Ecran - Ecran_simple.ino
 
+  ### Démonstration des quelques fonctions simplifiée pour la classe Ecran.
+
+
+  Note: Toutes les méthodes primitives de la classe Adafruit_SSD1306 sont disponibles dans la classe Ecran également.
+
+*/
 #include "Ecran.h"
 
-Ecran ecran;
+Ecran monEcran; //Création de l'objet 'ecran' de classe Ecran.
 void setup()
 {
     Serial.begin(115200);
 
-    ecran.begin();
-
-    // put your setup code here, to run once:
+    monEcran.begin(); // Initialiser l'écran.
 }
 
 void loop()
 {
+    monEcran.ecrire("Bonjour!"); //Écrire un texte dans l'écran.
+    delay(1000);
 
-    // put your main code here, to run repeatedly:
+    monEcran.ecrire("Comment allez-vous?", 2); //Écrire un texte dans l'écran, avec une taille de texte de 2.
+    delay(1000);
+
+    monEcran.ecrire("Moi,\n bien.", 3); //Écrire un texte dans l'écran, avec une taille de texte de 3.
+    delay(1000);
+
+    monEcran.effacer(); //Effacer tout l'écran
+    delay(2000);
+
+    monEcran.ecrire(" BOU!", 4); //Écrire un texte dans l'écran, avec une taille de texte de 4.
+    delay(1000);
+
+    for (int i = 0; i < 500; i++) //Boucle de 500 itérations:
+    {
+        int x = random(monEcran.width());  //Générer une valeur aléatoire située entre 0 et la largeur de l'écran.
+        int y = random(monEcran.height()); //Générer une valeur aléatoire située entre 0 et la hauteur de l'écran.
+
+        monEcran.dessinerPixel(x, y); //Afficher un pixel blanc à la coordonnée (x,y).
+    }
 }
